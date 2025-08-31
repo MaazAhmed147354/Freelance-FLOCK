4606983380167e79508626e1c8901be75db9ab4f
MaazAhmed147354 <maza147354@gmail.com>
2025-08-31 22:01:43 +0500

    Delete everything to Redesign

--- flockby-maaz/src/pages/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Home } from "./Home";
-export { default as Delivery } from "./Delivery";
-export { default as Checkout } from "./Checkout";
-export { default as Account } from "./Account";
-export { default as Contact } from "./Contact";
-export { default as Reviews } from "./Reviews";
